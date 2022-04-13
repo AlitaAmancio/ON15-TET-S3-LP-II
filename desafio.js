700579126aedafecbe771a0e795fcd999b97be2b
AlitaAmancio <amancioalita@gmail.com>
2022-04-13 20:47:10 -0300

    atualizações

--- desafio.js
+++ desafio.js
@@ -14,6 +14,7 @@ let clienteLilit = [
 let clienteMaria = [
     {produto: 'Cinto preto', valor: 22.0}, 
     {produto: 'Calça jeans clara', valor: 130.0},
+    {produto: 'Blusa simples', valor: 35.0},
     {produto: 'Blusa preta gola alta', valor: 60.0}, 
     {produto: 'Tênis casual preto', valor: 120.0}, 
     {produto: 'meia calça transparente', valor: 30.0}]
@@ -24,24 +25,25 @@ let clienteJoana = [
     {produto: 'Salto agulha 39', valor: 250.0}, 
     {produto: 'Jaqueta Jeans', valor: 300.0}, 
     {produto: 'Calça preta', valor: 100.0}, 
+    {produto: 'Calça jeans clara', valor: 130.0}, 
     {produto: 'Bolsa pequena', valor: 49.0}]
 
     
 function desconto(valor){
     if (valor >=50 && valor <=79){
-        console.log(`desconto de 5% aplicado`)
+        // console.log(`desconto de 5% aplicado`)
         return valor*0.05;
     }else if (valor >=80 && valor <= 99 ) {
-        console.log(`desconto de 10% aplicado`)
+        // console.log(`desconto de 10% aplicado`)
         return valor*0.1
     } else if (valor >=100 && valor <=199){
-        console.log(`desconto de 20% aplicado`)
+        // console.log(`desconto de 20% aplicado`)
         return valor*0.2
     } else if(valor >= 200){
-        console.log(`desconto de 50% aplicado`)
+        // console.log(`desconto de 50% aplicado`)
         return valor*0.5
     }else {
-        console.log(`sem descontos aplicáveis`)
+        // console.log(`sem descontos aplicáveis`)
         return 0
     }
 }
@@ -58,7 +60,7 @@ function compras(produtos){
     valorFinal = 0;
     totalPecas = 0;
     for(p of produtos){
-        console.log(p.produto);
+        // console.log(p.produto);
         totalPecas ++;
         valorTotal += p.valor;
         valorDesconto += desconto(p.valor);
@@ -74,10 +76,10 @@ function compras(produtos){
 
 console.log('Compras da Lilit')
 compras(clienteLilit);
-console.log('----------------')
-console.log('Compras da Maria')
-compras(clienteMaria);
-console.log('----------------')
-console.log('Compras da Joana')
-compras(clienteJoana);
-console.log('----------------')
+// console.log('----------------')
+// console.log('Compras da Maria')
+// compras(clienteMaria);
+// console.log('----------------')
+// console.log('Compras da Joana')
+// compras(clienteJoana);
+// console.log('----------------')
